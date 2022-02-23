48540c076e89ad37341f0ed6b4a8ba81554105fa
Eric Powers <ericdpowers@live.com>
2022-02-23 18:20:10 -0500

    Update Function Get-MTUFailurePoint.ps1

--- Function Get-MTUFailurePoint.ps1
+++ Function Get-MTUFailurePoint.ps1
@@ -1,77 +1,77 @@
 Function Get-MTUFailurePoint {
-[CMDLetBinding()]
-param(        
-[Parameter(Mandatory = $true,
-ValueFromPipelineByPropertyName = $true,
-Position = 0)] 
-$ComputerName,
-[Switch]$AutoTuneMTU )
-$ComputerName = $ComputerName.ToUpper()
-$MTUfailurePoint = $null
-$Adjustment = [pscustomobject]@{
-    Source = $env:COMPUTERNAME
-    Target = $ComputerName
-    FragmentationRequired = $false
-    LastMTUSuccess = $null
-    MTUFailureSize = $null
-    RecommendedMTUSize = $null
-    AutoTuneMTU =  $AutoTuneMTU
+    [CMDLetBinding()]
+    param(        
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0)] 
+        $ComputerName,
+        [Switch]$AutoTuneMTU )
+    $ComputerName = $ComputerName.ToUpper()
+    $MTUfailurePoint = $null
+    $Adjustment = [pscustomobject]@{
+        Source                = $env:COMPUTERNAME
+        Target                = $ComputerName
+        FragmentationRequired = $false
+        LastMTUSuccess        = $null
+        MTUFailureSize        = $null
+        RecommendedMTUSize    = $null
+        AutoTuneMTU           = $AutoTuneMTU
 
-}
+    }
 
-#Warm up
-Write-Progress -Activity "Calculating Maximum MTU Size for communication with $Computername" -Status "Checking system availability" -PercentComplete 0
-$SystemIsAvailable =  Test-Connection -ComputerName $ComputerName -ErrorAction SilentlyContinue
+    #Warm up
+    Write-Progress -Activity "Calculating Maximum MTU Size for communication with $Computername" -Status "Checking system availability" -PercentComplete 0
+    $SystemIsAvailable = Test-Connection -ComputerName $ComputerName -ErrorAction SilentlyContinue
 
-    if($SystemIsAvailable){
+    if ($SystemIsAvailable) {
         Write-Progress -Activity "Calculating Maximum MTU Size for communication with $Computername" -Status "Confirmed system is available" -PercentComplete 5
         $packetsize = 500
         $PercentComplete = 10 
 
-        do{
+        do {
             Write-Progress -Activity "Calculating Maximum MTU Size for communications with $Computername" -Status "Testing MTU size:$packetsize" -PercentComplete  $PercentComplete
 
             $PingRes = ping $ComputerName -l $packetsize -f -n 2 
-            if( [string](($PingRes -split "\n")[2..3])  -match "Reply from"){
+            if ( [string](($PingRes -split "\n")[2..3]) -match "Reply from") {
                 $SuccessfullMTU = $packetsize
             }
-            else{
-                $MTUfailurePoint =  $packetsize
+            else {
+                $MTUfailurePoint = $packetsize
             }
             $PercentComplete = $PercentComplete + 5
             Write-Progress -Activity "Calculating Maximum MTU Size for communications with $Computername" -Status "Testing MTU size:$packetsize; Status: $([string](($PingRes -split "\n")[2..3])  -match "Reply from")" -PercentComplete $PercentComplete 
-            $packetsize =  ($packetsize + 100)
+            $packetsize = ($packetsize + 100)
 
-        }while(($packetsize  -lt 1600) -and !$MTUfailurePoint)
+        }while (($packetsize -lt 1600) -and !$MTUfailurePoint)
 
              
         $PercentComplete = $PercentComplete + 5
         Write-Progress -Activity "Calculating Maximum MTU Size for communications with $Computername" -Status "Failure occurred at Maximum MTU size: $MTUfailurePoint" -PercentComplete  $PercentComplete
         Start-Sleep 1
             
-        do{
+        do {
             $PercentComplete = $PercentComplete + 5
             Write-Progress -Activity "Calculating Maximum MTU Size for communications with $Computername" -Status "Gauging a better Maximum MTU size: $MTUfailurePoint" -PercentComplete  $PercentComplete
             $PingRes = ping $ComputerName -l $MTUfailurePoint -f -n 2 
-            if( [string](($PingRes -split "\n")[2..3])  -match "Reply from"){
-                    $FinetunedMTU = $MTUfailurePoint
+            if ( [string](($PingRes -split "\n")[2..3]) -match "Reply from") {
+                $FinetunedMTU = $MTUfailurePoint
             }
-            Else{
+            Else {
                 $MTUfailurePoint = $MTUfailurePoint - 10
             }
 
         }until($FinetunedMTU)
 
         $MTUfailurePoint = $FinetunedMTU
-        Do{
+        Do {
             $PercentComplete = $PercentComplete + 5
             Write-Progress -Activity "Calculating Maximum MTU Size for communications with $Computername" -Status "Fine Tuning a better Maximum MTU size: $MTUfailurePoint" -PercentComplete  $PercentComplete
             $PingRes = ping $ComputerName -l $FinetunedMTU -f -n 2 
-            if( [string](($PingRes -split "\n")[2..3])  -notmatch "Reply from"){
+            if ( [string](($PingRes -split "\n")[2..3]) -notmatch "Reply from") {
                 $FinetunedMTU = $MTUfailurePoint
             }
-            Else{
-                $MTUfailurePoint = $MTUfailurePoint  + 5
+            Else {
+                $MTUfailurePoint = $MTUfailurePoint + 5
             }
                 
             Write-Progress -Activity "Calculating Maximum MTU Size for communications with $Computername" -Status "Fine Tuning a better Maximum MTU size: $MTUfailurePoint" -PercentComplete  $PercentComplete
@@ -81,21 +81,21 @@ $SystemIsAvailable =  Test-Connection -ComputerName $ComputerName -ErrorAction S
         $PercentComplete = 100
         Write-Progress -Activity "Calculating Maximum MTU Size for communications with $Computername" -Status "Optimal Maximum MTU size: $FinetunedMTU" -PercentComplete  $PercentComplete
 
-        [array]$AdaptersRequiringConfiguration = (get-NetIPInterface  -ErrorAction SilentlyContinue | where {$_.ifIndex -ne 1 -and $_.NlMtu -gt $FinetunedMTU -and $_.ConnectionState -eq 'Connected'})
+        [array]$AdaptersRequiringConfiguration = (Get-NetIPInterface  -ErrorAction SilentlyContinue | where { $_.ifIndex -ne 1 -and $_.NlMtu -gt $FinetunedMTU -and $_.ConnectionState -eq 'Connected' })
         $Adjustment.FragmentationRequired = $AdaptersRequiringConfiguration.Count -ne 0
         $Adjustment.LastMTUSuccess = $FinetunedMTU
         $Adjustment.MTUFailureSize = $MTUfailurePoint 
         $Adjustment.RecommendedMTUSize = ($FinetunedMTU - 20)
 
-        if($AutoTuneMTU -and $AdaptersRequiringConfiguration -ne $Adjustment.FragmentationRequired){
+        if ($AutoTuneMTU -and $AdaptersRequiringConfiguration -ne $Adjustment.FragmentationRequired) {
             $Adjustment | Add-Member -Name SuccessfullyConfigured -MemberType NoteProperty -Value $true
             
-            Foreach ($Adapter in $AdaptersRequiringConfiguration){
+            Foreach ($Adapter in $AdaptersRequiringConfiguration) {
 
-                Try{
-                        Set-NetIPInterface -InterfaceIndex $($adapter.ifIndex)  -NlMtuBytes $Adjustment.RecommendedMTUSize
+                Try {
+                    Set-NetIPInterface -InterfaceIndex $($adapter.ifIndex)  -NlMtuBytes $Adjustment.RecommendedMTUSize
                 }
-                Catch{
+                Catch {
                     $Adjustment.SuccessfullyConfigured = $False
                 }
 
@@ -103,26 +103,26 @@ $SystemIsAvailable =  Test-Connection -ComputerName $ComputerName -ErrorAction S
         }
 
 
-        if($Adjustment.AutoTuneMTU -and $Adjustment.FragmentationRequired){
-            if( $Adjustment.SuccessfullyConfigured){
+        if ($Adjustment.AutoTuneMTU -and $Adjustment.FragmentationRequired) {
+            if ( $Adjustment.SuccessfullyConfigured) {
                 $Adjustment
-                Write-host -ForegroundColor Green "Successfully adjusted the MTU size for " -nonewline; Write-host $env:COMPUTERNAME -nonewline; Write-host -ForegroundColor Green " to " -nonewline; Write-host  $($Adjustment.RecommendedMTUSize )
+                Write-Host -ForegroundColor Green "Successfully adjusted the MTU size for " -nonewline; Write-Host $env:COMPUTERNAME -nonewline; Write-Host -ForegroundColor Green " to " -nonewline; Write-Host  $($Adjustment.RecommendedMTUSize )
             }
-            Else{
+            Else {
                 $Adjustment
-                Write-host -ForegroundColor Red "Failed to adjust the MTU size for " -nonewline; Write-host $env:COMPUTERNAME -nonewline; Write-host -ForegroundColor Green " to " -nonewline; Write-host  $($Adjustment.RecommendedMTUSize )
+                Write-Host -ForegroundColor Red "Failed to adjust the MTU size for " -nonewline; Write-Host $env:COMPUTERNAME -nonewline; Write-Host -ForegroundColor Green " to " -nonewline; Write-Host  $($Adjustment.RecommendedMTUSize )
             }
         }
-        Elseif(!$AutoTuneMTU  -and $Adjustment.FragmentationRequired){
-                $Adjustment
-                Write-host -ForegroundColor Yellow "Recommned adjusting the MTU size for " -nonewline; Write-host $env:COMPUTERNAME -nonewline; Write-host -ForegroundColor Yellow " to " -nonewline; Write-host  $($Adjustment.RecommendedMTUSize )
+        Elseif (!$AutoTuneMTU -and $Adjustment.FragmentationRequired) {
+            $Adjustment
+            Write-Host -ForegroundColor Yellow "Recommned adjusting the MTU size for " -nonewline; Write-Host $env:COMPUTERNAME -nonewline; Write-Host -ForegroundColor Yellow " to " -nonewline; Write-Host  $($Adjustment.RecommendedMTUSize )
         }
-        Else{
-                $Adjustment
+        Else {
+            $Adjustment
         }
 
     }    
-    Else{
+    Else {
         Write-Error -Exception "TCP Connection" -Message "Unable to reach host $ComputerName" -Category ConnectionError -RecommendedAction "Check the Computer Name or IP address of the Target System" 
     }
 
